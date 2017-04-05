d43abdf95b60fc847c50e2734f25cfdd4958f2da
lebashir <bashir.abihabib@gmail.com>
2017-04-04 20:59:58 -0500

    completed assignment-15-models

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Comment, type: :model do
   let(:comment) {Comment.create!(body: 'Comment Body', post: post) }
   describe "attributes" do
     it "has a body attribute" do
-      expect(comment).to have_attributes(body:"Comment Body")
+      expect(comment).to have_attributes(body:"Comment Body"
     end
   end
 end
