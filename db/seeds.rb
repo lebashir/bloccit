e5d382292dfbd08832dddfdc297bf40ab60df94d
lebashir <bashir.abihabib@gmail.com>
2017-04-24 15:04:53 +0300

    completed assignment-16-seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ require 'random_data'
    )
  end
  posts = Post.all
- 
+
  # Create Comments
  100.times do
    Comment.create!(
@@ -16,7 +16,8 @@ require 'random_data'
      body: RandomData.random_paragraph
    )
  end
-
+Post.find_or_create_by(title: "What is love?", body: "Baby don't hurt me!")
+Comment.find_or_create_by(body:"Don't hurt me, no more")
  puts "Seed finished"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
