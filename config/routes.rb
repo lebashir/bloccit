389159cbc9c3336655a1d009dd8cb29f9ed20be2
lebashir <bashir.abihabib@gmail.com>
2017-05-18 21:14:46 -0500

    finished the topics section of checkpoint 21

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  resources :topics
   resources :posts
 
   get 'about' => 'welcome#about'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,22 @@
 require 'random_data'
-
+ # Create topics
+ 15.times do
+   Topic.create!(
+    name: RandomData.random_sentence,
+    description: RandomData.random_paragraph
+   )
+ end
+ topics = Topic.all
  # Create Posts
  50.times do
    Post.create!(
+     topic: topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
  end
  posts = Post.all
- 
+
  # Create Comments
  100.times do
    Comment.create!(
@@ -18,5 +26,6 @@ require 'random_data'
  end
 
  puts "Seed finished"
+ puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
